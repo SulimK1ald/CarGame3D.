999e9b02855ba3499af37ce1d48aa12362b34ae2
Sulim Azarsaev <96692012+SulimK1ald@users.noreply.github.com>
2023-04-18 07:26:14 +0300

    Add files via upload

---
+++ Scripts/EnemyBox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class EnemyBox : MonoBehaviour {
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter(Collider boxCollider)
+	{
+		if(boxCollider.name == "Car")
+		{
+			SceneManager.LoadScene("lesson7");
+		}
+	}
+}
